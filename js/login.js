b2db2866791501cc79429b642bfb3d9cf733748f
Hugo <hhldiniz@gmail.com>
2016-10-18 11:53:47 -0300

    adicionando ajax para página de login

--- js/login.js
+++ js/login.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-	
+	$("#login").click(function(){
+		$.ajax({
+			  url: 'localhost/login.php',
+			  accepts: {
+			    Accept: 'application/vnd.github.v3+json',
+			  },
+			  dataType: 'mycustomtype'
+		});
+	});
 });
